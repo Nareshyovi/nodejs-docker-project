61e62d03a8a42a1a18f86b70b1a70dab38f6b451
root <root@ip-172-31-39-247.ap-south-1.compute.internal>
2026-07-09 10:27:18 +0000

    app.js correct

--- app.js
+++ app.js
@@ -1,16 +1,14 @@
-// Select elements from the DOM
-const button = document.getElementById('action-btn');
-const title = document.getElementById('main-title');
-const description = document.getElementById('description');
+const express = require('express');
 
-// Add a click event listener to the button
-button.addEventListener('click', () => {
-    // Change text content
-    title.textContent = "Hello from app.js!";
-    description.textContent = "The code executed successfully and modified the website live.";
-    
-    // Change style dynamically
-    title.style.color = "#28a745"; 
-    button.style.backgroundColor = "#28a745";
-    button.textContent = "Success!";
+const app = express();
+
+const PORT = 3000;
+
+app.get('/', (req, res) => {
+    res.send('<h1>Hello from Docker!</h1>');
 });
+
+app.listen(PORT, () => {
+    console.log(`Server running on ${PORT}`);
+});
+

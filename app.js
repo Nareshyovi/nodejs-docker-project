81af0d60823842a03f0d1d986023b6c297c081d8
root <root@ip-172-31-39-247.ap-south-1.compute.internal>
2026-07-09 09:24:45 +0000

    push

---
+++ app.js
@@ -0,0 +1,14 @@
+const express = require('express');
+
+const app = express();
+
+const PORT = 3000;
+
+app.get('/', (req, res) => {
+    res.send('<h1>Hello from Docker!</h1>');
+});
+
+app.listen(PORT, () => {
+    console.log(`Server running on ${PORT}`);
+});
+
